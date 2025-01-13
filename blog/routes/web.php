6c5f5cd8668733230303b3ea71582de6b7e84f72
min khant <minkhant4287@gmail.com>
2025-01-13 19:29:28 +0630

    Day-1 finish Routing And Controller

---
+++ blog/routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\Product\ProductController;
+Route::get('/',[ArticleController::class, 'index']);
+Route::get('/articles',[ArticleController::class, 'index']);
+Route::get('/articles/detail/{id}', [ArticleController::class, 'detail']);
+Route::get('/products',[ProductController::class,'index']);
